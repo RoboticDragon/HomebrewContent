1b02a0563d292ae0a36f2d67bd23069ecdea4d3c
RoboticDragon <hilaryrlutz@gmail.com>
2025-02-26 13:08:41 -0500

    Create GrimHollowTrans.js

---
+++ GrimHollowTrans.js
@@ -0,0 +1,11 @@
+var iFileName = "GH-Transformations.js";
+
+RequiredSheetVersion(13);
+// This file adds the content from the Grim Hollows Player Guide
+
+// Define the sources
+SourceList["GH"] = {
+	name : "Grim Hollows Player Guide",
+	abbreviation : "GH",
+	group : "Third Party",
+};
