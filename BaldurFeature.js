92ff8c93c1d58b3fa144a35bde3adae810b14ada
RoboticDragon <hilaryrlutz@gmail.com>
2025-03-04 11:15:48 -0500

    Create BaldurFeature.js

---
+++ BaldurFeature.js
@@ -0,0 +1,10 @@
+var iFileName = "Baldur's Gate Magic Items.js";
+
+MagicItemsList["The Dancing Breeze"] = {
+source : ["HB", 0],
+  type = "weapon",
+  rarity: "very rare",
+  weight: 5.4,
+  description: "+2 finesse glaive with a bonus attack option",
+  
+};
