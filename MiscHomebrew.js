d0dffeac71437fbc070bcd81577d82695889c243
RoboticDragon <hilaryrlutz@gmail.com>
2025-04-02 14:56:23 -0400

    Update MiscHomebrew.js

--- MiscHomebrew.js
+++ MiscHomebrew.js
@@ -1,8 +1,8 @@
 var iFileName = "MiscHomebrew.js";
-SourceList["BoP"] = {
+SourceList["MiscHB"] = {
 name : "MiscHomebrew",
   abbreviation : "MiscHB",
-}
+};
 //This is basically the Satyr's Flute from The Gates of the Shadowmage by Eldritch Press but modified.
 MagicItemsList["Twin Tailed Flute"] = {
   name : "Twin Tailed Flute",
@@ -15,7 +15,10 @@ MagicItemsList["Twin Tailed Flute"] = {
 		return v.isSpellcaster;
 	},
 	description : "I can use an action to cast a spell from the flute or to play a special song from the flute in order to cause one of a set of listed effects. Once used this way, the flute can't be used to play a special song until the next dusk.",
-  descriptionLong : ["This simple but fantastical woodwind instrument was crafted by a celestial fae, who used it to enhance his magic after losing his voice.",
+  	action: ["action", " Play song"],
+	toNotesPage : [{
+	name : "Songs",  
+	note : ["This simple but fantastical woodwind instrument was crafted by a celestial fae, who used it to enhance his magic after losing his voice.",
 		    "Its alluring melody can affect the minds of creatures around it. While holding it, I gain a +2 bonus to spell save DCs and spell attack rolls",
 		    "As an action I can play one of the following tunes. Each creature of my choice within 60 feet of me that can hear the flute must succeed on a wisdom saving throw against my spell DC or succumb to the tune’s effects for 1 minute.",
 		    "I can spend a spell slot to extend the duration for a minute per spell level.",
@@ -24,10 +27,39 @@ MagicItemsList["Twin Tailed Flute"] = {
 		    "Stupefying Aria: An affected target is stunned until the effect ends or someone uses an action to shake the target out of its stupor.",
 		    "Horrifying Dirge: An affected target is frightened until the effect ends.",
 		    "Infuriating Cadenza: An affected target uses its turn to move toward a randomly determined creature and attack it. If there are no creatures within its sight, the target does nothing. Each time a target takes damage, it makes a new Wisdom saving throw against the effect. If the saving throw succeeds, the effect ends.",
-		    "Soothing Lullaby: An affected target falls unconscious until the effect ends, the target takes damage, or someone uses an action to shake or slap it awake. This song can effect elves, although they have advantage on their saves"],
-	spellCalc : [
-		function (type) {
-			if (type == "dc" || type == "attack") return 2;
+		    "Soothing Lullaby: An affected target falls unconscious until the effect ends, the target takes damage, or someone uses an action to shake or slap it awake. This song can effect elves, although they have advantage on their saves"
+		    ]
+	  
+  }],
+	extraLimitedFeatures : [{
+			name : "Alluring Sonata",
+			usages : 1,
+			recovery : "dusk"
+		}, {
+			name : "Stupefying Aria",
+			usages : 1,
+			recovery : "dusk"
+		}, {
+			name : "Horrifying Dirge",
+			usages : 1,
+			recovery : "dusk"
 		},
-	]
-}
+			       {
+			name : "Infuriating Cadenza",
+			usages : 1,
+			recovery : "dusk"
+		},
+			       {
+			name : "Soothing Lullaby",
+			usages : 1,
+			recovery : "dusk"
+		}],
+			calcChanges : {
+			spellCalc : [
+				function (type, spellcasters, ability) {
+					if (type != "prepare") return 2;
+				},
+				"While using the Twined Tail Flute my spell DC and spell attack modifier increase by 2."
+			]
+		}
+};
