f9815f3ea81ef4edbd959f8de0e8e8ca53e2eb0f
Hilary Lutz <hilaryrlutz@gmail.com>
2025-12-16 15:20:05 -0500

    Create GalateanRaces.js

---
+++ GalateanRaces.js
@@ -0,0 +1,35 @@
+var iFileName = "GalateanRaces.js";
+SourceList["GalaR"] = {
+name : "GalateanRaces",
+  abbreviation : "GRaces",
+};
+
+RaceList["spiderfolk"] = {
+	regExpSearch : /^(?=.*spider)(?=.*folk).*$/i,
+	name : "Spiderfolk",
+	source : [["GalaR", 8]],
+	plural : "Spiderfolk",
+	size : 3,
+	speed : {
+		walk : { spd : 35, enc : 25 }
+	},
+	scoresGeneric : true,
+	trait : "Spiderfolk" + (typePF ? "\n " : "\t") +
+	"\u2022 Chitinous Shell: When I'm not wearing armor, my AC is equal to 13 + my dex modifier. I can wear a shield and still gain this benefit." +
+	"\n \u2022 Spider Climb: Climbing speed equal to walking speed. At 3rd level, I can move up, down, and across vertical surfaces and upside down along ceilings, while leaving my hands free."
+	features : {
+		"spider climb" : { // So it doesn't interfere with inherited speeds by useFromPreviousRace
+			name : "Spider Climb",
+			minlevel : 1,
+			speed : { climb : { spd : "walk", enc : "walk" } }
+		}
+	},
+	vision : [["Darkvision", 60]],
+	armorOptions : [{
+		regExpSearch : /^(?=.*carapace)(?=.*chameleon).*$/i,
+		name : "Chitinous Shell",
+		source : [["GalaR", 8]],
+		ac : 13,
+		selectNow : true
+	}],
+};
